b49605fd6b98295377de238047d8e2e46d845bab
KhanyisoCairo <Khanyisoyezo12@gmail.com>
2020-02-21 13:09:51 +0200

    Deleted unused folders

--- test/bicycles/models/TandemTest.java
+++ test/bicycles/models/TandemTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TendemTest {
+public class TandemTest {
   @Test
     public  void ShouldAccelerate(){
       Tandem tdm = new Tandem();
